585e990779d9cfe0bc420b5bbd5e6117f291bb96
BeatEhrmann <beat.ehrmann@uni-konstanz.de>
2024-12-11 13:06:32 +0100

    caluclate aproximal read time in seconds also for frame reduced data

--- MeTrEx/gui.py
+++ MeTrEx/gui.py
@@ -2243,7 +2243,7 @@ class MenuAction(QAction):
             # Get CPU frequency for speed estimation
             cpu_freq = psutil.cpu_freq().current + 0.0001
             # caluclate aproximal read time in seconds
-            read_time = round((data_file_size * 700) / (cpu_freq * 10**6))
+            read_time = round(((data_file_size / self.parent.data.k) * 700) / (cpu_freq * 10**6))
             if platform.system() == 'Windows':
                 read_time = read_time * 4
                 
